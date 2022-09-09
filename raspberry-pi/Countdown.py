57445134d1f564047fdb50fdf292fd284667ed9a
gdaless20 <gdaless20@charlottesvilleschools.org>
2022-09-09 10:46:26 -0400

    tyhujik

---
+++ raspberry-pi/Countdown.py
@@ -0,0 +1,5 @@
+import time
+for x in range(10,0,-1):  #counting range of numbers
+    print(x) 
+    time.sleep (1)   #rest 1 sec for a pause
+print("LAUNCHY")     #say dis
---
+++ raspberry-pi/LED_blink.py
@@ -0,0 +1,7 @@
+for x in range(10,0):
+  print(x)
+  var x = setInterval(function)() 
+if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("demo").innerHTML = "EXPIRED";
+  }
