e1f638b5b586754a62568d25abc0b0edb0cb319c
Gabriella <71349797+gdaless20@users.noreply.github.com>
2022-09-13 11:43:55 -0400

    Update Countdown.py

--- raspberry-pi/Countdown.py
+++ raspberry-pi/Countdown.py
@@ -1,5 +1,5 @@
-#import time
+import time    #time variable
 for x in range(10,0,-1):  #counting range of numbers
-    print(x)         #
+    print(x)         #says each number
     time.sleep (1)   #rest 1 sec for a pause
 print("LAUNCHY")     #say this at the end of 10s
